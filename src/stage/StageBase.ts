3906a39b0dd379d0f0d22a3a5a45e2872e47ab7c
cali4nyaguy-alt <cali4nyaguy@gmail.com>
2026-04-05 10:27:54 -0700

    Create StageBase.ts

---
+++ src/stage/StageBase.ts
@@ -0,0 +1,34 @@
+import { StageContext } from "./types";
+import { Message, createMessage } from "./Message";
+import { Environment } from "./Environment";
+
+export abstract class StageBase<
+  InitState = any,
+  ChatState = any,
+  MessageState = any,
+  ConfigState = any
+> {
+  ctx: StageContext;
+  env: Environment;
+
+  constructor(initial: InitState, chat: ChatState, config: ConfigState) {
+    this.ctx = {
+      init: initial,
+      chat,
+      message: {},
+      config,
+    };
+    this.env = new Environment(this.ctx);
+  }
+
+  abstract load(): Promise<void> | void;
+  abstract onUserMessage(msg: string): Promise<Message[]> | Message[];
+
+  protected reply(text: string): Message {
+    return createMessage("assistant", text);
+  }
+
+  protected user(text: string): Message {
+    return createMessage("user", text);
+  }
+}
