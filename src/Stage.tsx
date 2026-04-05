81ebd8372028bc9c645ffc6a12d55ce2351909af
cali4nyaguy-alt <cali4nyaguy@gmail.com>
2026-04-04 23:38:52 -0700

    Update Stage.tsx

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -78,7 +78,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
     this.myInternalState['bloodlust'] = 0;
     this.myInternalState['kan'] = 0;
     this.myInternalState['respect'] = 0;
-  } /} / This is the closing bracket that was on line 77
+  } / This is the closing bracket that was on line 77
  
     
 
